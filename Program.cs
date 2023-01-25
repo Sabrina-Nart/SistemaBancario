4d773202afea0d4e7af5957b6f2ea1c2955f71ce
sabri <sabri@DESKTOP-GSG041J>
2023-01-24 21:38:47 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using SistemaBancario.SistemaBancarioTeste;
+using System;
+
+namespace SistemaBancario
+{
+    public class Program
+    {
+        public static void Main(String[] args)
+        {
+            var conta = new Conta();
+            conta.NomeConta = "ContaTeste";
+            conta.NumeroConta = 1000;
+            conta.Agencia = 001;
+            conta.Saldo = 1000;
+
+            conta.Titular = new Titular()
+            {
+                Cpf = "05689525625",
+                Rg = "8954566",
+                Endereco = "Rua Centro",
+                Nome = "Ramon"
+            };
+
+            Console.WriteLine($"O saldo atual é de { conta.verSaldo()}");
+
+            conta.Depositar(150);
+
+            Console.WriteLine($"O saldo atual, após o deposito é de {conta.verSaldo()}");
+
+            conta.Sacar(900);
+
+            Console.WriteLine($"O saldo atual, após o saque é de {conta.verSaldo()}");
+
+            conta.Sacar(2000);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SistemaBancarioTeste/Conta.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBancario.SistemaBancarioTeste
+{
+    public class Conta
+    {
+        public int Agencia { get; set; }
+        public int NumeroConta { get; set; }    
+        public string NomeConta { get; set; }
+        public Titular Titular { get; set; }
+        public Decimal Saldo { get; set; }
+
+        public Decimal verSaldo()
+        {
+            return Saldo;
+        }
+
+        public void Depositar(decimal valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Sacar(decimal valor)
+        {
+            if (valor <= Saldo)
+                Saldo -= valor;
+            else
+                Console.WriteLine("Saque não pode ser realizado, pois não há saldo suficiente");
+        }
+
+    }
+}
